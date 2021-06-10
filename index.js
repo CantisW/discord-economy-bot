9bcadb0292ae68be01f9e37698a41f22d873fd8f
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2021-06-10 10:21:07 -0700

    FIX: Cooldown, add aliases, status

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ import Discord from "discord.js"
 const client = new Discord.Client();
 
 import fs from "fs"
-import crypto from "crypto";
 
 import settings from './data/bot-settings.json';
-const { prefix, token, message } = settings;
+const { prefix, token, status } = settings;
 
 client.commands = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
+
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-    const command = await import(`./commands/${file}`);
-    client.commands.set(command.name, command); 
-    console.log("Command loaded: "+command.name);
+    const command = await import(`./commands/${file}`); // semi-hacky way to allow dynamic imports until ES6 actually allows dynamic imports
+    client.commands.set(command.name, command);
+    console.log("Command loaded: " + command.name);
 }
 
 
@@ -22,7 +23,7 @@ client.on('ready', () => {
     client.user.setPresence({
         status: "online",
         activity: {
-            name: `${prefix} | ${message}`,
+            name: `${prefix} | ${status}`,
             type: "PLAYING"
         }
     });
@@ -32,14 +33,48 @@ client.on('message', msg => {
     if (!msg.content.startsWith(prefix) || msg.author.bot) return; // Check if command starts with prefix or is sent by bot
 
     const args = msg.content.slice(prefix.length).trim().split(/ +/); // Get content, remove prefix, remove spaces at start and end, and split where there's a space
-    const command = args.shift().toLowerCase(); // removes first from "args" and returns it as "command"
+    const commandName = args.shift().toLowerCase(); // removes first from "args" and returns it as "command"
 
-    if(!client.commands.has(command)) return; // Does this command exist?
+    const commandObj = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
 
-    try {
-        client.commands.get(command).execute(msg,args); // msg passes message object so that you can send messages through command files
-    } catch (err){
-        console.error(err);
+    if (!commandObj) return; // Does this command exist?
+
+    let command = commandObj.name // to lazy to edit current code so this works for now
+    let cooldown = client.commands.get(command).cooldown * 1000 || 3 * 1000;
+
+    // this is a REALLY hacky way to do cooldowns
+    // but it works so whatever
+
+    let cooldown_token = msg.author.id+"."+command
+    
+    if (client.cooldowns.has(cooldown_token) && client.cooldowns.get(cooldown_token) == cooldown) { // Cooldown code
+        let time = client.commands.get(command).cooldown || 3;
+        //
+        // dynamic cooldown
+        //
+        if (cooldown/1000 >= 60) {
+            msg.reply(`please wait ${time/60} minutes!`);
+        } else {
+            msg.reply(`please wait ${time} seconds!`);
+        }
+    } else {
+        client.cooldowns.set(cooldown_token, cooldown);
+        setTimeout(() => {
+            for (const [key, value] of client.cooldowns.entries()) {
+                if (key == cooldown_token && value == cooldown) {
+                    client.cooldowns.delete(key);
+                }
+            }
+            
+        }, cooldown)
+        //
+        // Command handler
+        //
+        try {
+            client.commands.get(command).execute(msg, args); // msg passes message object so that you can send messages through command files
+        } catch (err) {
+            console.error(err);
+        }
     }
 });
 
