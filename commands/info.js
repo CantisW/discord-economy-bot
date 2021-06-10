d21a87e16e478a50601b0543b9450d8de78b5450
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2021-06-09 17:14:41 -0700

    Finish info command

--- commands/info.js
+++ commands/info.js
@@ -1,19 +1,24 @@
 import Discord from "discord.js";
+import fs from "fs";
 
 import config from "../data/config.json";
-const { coinName, ticker, maxsupply, blockreward, exchangerate, currency, decimals, txfee } = config;
+import { getSupply, parseDecimals, getConfig } from "../util/economy-blockchain.js";
+const { coinName, ticker, maxsupply, blockreward, currency, decimals, txfee } = config;
 
 export const name = "info";
 export const description = `Get info about ${ticker}`;
 
 export const execute = (message,args) => {
-	getInfo();
-	message.channel.send(infoEmbed);
+	message.channel.send(getInfo());
 };
 
 const getInfo = () => {
-	let marketcap = 0;
-	let supply = 0;
+	let config = getConfig();
+
+	let exchangerate = config.exchangerate;
+	let marketcap = maxsupply*exchangerate;
+	let supply = getSupply();
+
     const infoEmbed = new Discord.MessageEmbed()
 		.setColor('#0099ff')
 		.setTitle(`${coinName} (${ticker})`)
@@ -23,7 +28,7 @@ const getInfo = () => {
 		//.setThumbnail('')
 		.addFields(
 			{ name: 'Max Supply', value: `${maxsupply}` },
-			{ name: 'Exchange Rate', value: `1 ${ticker} for ${exchangerate} ${currency}.\n1 iron for ${Math.round((1/exchangerate) * (10**decimals))/10**decimals} ${ticker}.` },
+			{ name: 'Exchange Rate', value: `1 ${ticker} for ${exchangerate} ${currency}.\n1 ${currency} for ${parseDecimals(1/exchangerate)} ${ticker}.` },
 			{ name: 'Current Supply', value: `${supply}`, inline: true },
 			{ name: 'Block Reward', value: `${blockreward}`, inline: true },
   	 		{ name: 'Market Cap', value: `${marketcap} ${currency}`},
@@ -35,5 +40,5 @@ const getInfo = () => {
 		//.setImage('')
 		.setTimestamp()
 		.setFooter(`${coinName} (${ticker})`, ''); // TODO: set url as second arg
+	return infoEmbed;
 }
-
