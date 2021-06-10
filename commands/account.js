6b1b78e6e6b930bffc3f68ee052d1b62c698268c
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2021-06-09 17:46:23 -0700

    Add account handling

--- commands/account.js
+++ commands/account.js
@@ -1,6 +1,13 @@
+import * as accounts from "../util/economy-accounts.js" 
+
 export const name = "account";
 export const description = "Create an account.";
 
 export const execute = (message,args) => {
-
+    if (accounts.checkIfAccountExists(message.author.id)) {
+        message.reply("you already have an account!")
+    } else {
+        accounts.createAccount(message.author.id)
+        message.reply("your account has been created.")
+    }
 };
