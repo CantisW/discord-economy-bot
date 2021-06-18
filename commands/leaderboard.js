12f403e294f0a470a6cd5f657052e60bd22f7e1e
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2021-06-17 20:33:59 -0700

    Implement leaderboard sort

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,10 +1,11 @@
 import { RichEmbed } from "@guildedjs/guilded.js";
-import { getSupply, getUsers, parseDecimals } from "../util/economy-blockchain.js";
+import { getSupply, getUsers, parseDecimals, insertionSort, getIDFromBalance } from "../util/economy-blockchain.js";
 import config from "../data/config.json";
 const { coinName, ticker } = config;
 
 export const name = "leaderboard";
 export const description = "Get the global leaderboard.";
+export const aliases = ["lb"]
 
 export const execute = (message, args) => {
     message.channel.send(getLeaderboard());
@@ -12,25 +13,35 @@ export const execute = (message, args) => {
 
 const getLeaderboard = () => {
     let users = getUsers();
-
     var count = Object.keys(users.accounts).length;
 
     let supply = getSupply();
+    let balArray = [];
+
+    for (let i = 0; i < count; i++) {
+        balArray.push(parseDecimals(users.accounts[i].balance))
+    }
+    insertionSort(balArray)
 
     const leaderboardEmbed = new RichEmbed()
         .setColor('0xf1c40f')
         .setTitle(`${coinName} (${ticker})`)
         //.setURL('')
         //.setAuthor('Santeeisweird9')
-        .setDescription(`The rich list of ${name}!`)
+        .setDescription(`The rich list of ${coinName}!`)
         //.setThumbnail('')
         //.addField('', '', true)
         //.setImage('')
         .setTimestamp()
         .setFooter(`${name} (${ticker})`, ''); // TODO: set url as second arg
 
-    for (let i = 0; i < count; i++) {
-        leaderboardEmbed.addFields({ name: `${users.accounts[i].userid}`, value: `${parseDecimals(users.accounts[i].balance)} ${ticker} (${parseDecimals((users.accounts[i].balance / supply)*100)}% of supply)` })
-    }
+        for (let i = 0; i < count; i++) {
+            leaderboardEmbed.addFields(
+                { 
+                    name: `${getIDFromBalance(balArray[i])}`, 
+                    value: `${balArray[i]} ${ticker} (${parseDecimals((balArray[i] / supply)*100)}% of supply)` 
+                }
+            )
+        }
     return leaderboardEmbed;
 }
