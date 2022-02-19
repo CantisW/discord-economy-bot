479d4423083e7ae458e6045d7c7a3a7759dc735b
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2022-02-19 13:28:31 -0800

    add: bot util

--- src/util/bot.ts
+++ src/util/bot.ts
@@ -1,18 +1,27 @@
 import fs from "fs";
-import { IUser } from "./types.js";
+import { IConfig } from "./types";
 
-export const getUsers = (): IUser => {
-    let users = fs.readFileSync("./src/data/accounts.json","utf-8");
-    return JSON.parse(users);
-}
-
-export const getConfig = (): object => {
+export const getConfig = (): IConfig => {
     let config = fs.readFileSync("./src/data/settings.json","utf-8");
     return JSON.parse(config);
+}
+
+export const WriteToConfig = (property: string, write: string | number) => {
+    let config = getConfig()
+
+    config[property] = write;
+
+    fs.writeFile("./src/data/settings.json", JSON.stringify(config, null, 2), (err) => {
+        if (err) console.log(err);
+    });
 }
 
 export const returnSetting = (setting: string) => {
     let bot = fs.readFileSync("./src/data/bot.json","utf-8");
     let settings = JSON.parse(bot);
     return setting ? settings[setting] : settings
+}
+
+export const sanitizeId = (id: string) => {
+    return id.replace(/[\\<>@#&!]/g, "");
 }
