0b91d031e67d6c987246ee10e27db9557df50124
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2022-02-19 13:28:55 -0800

    chore: update typings

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,23 +1,20 @@
 export interface IConfig {
+    [key: string]: string | number,
     coinName: string,
     ticker: string,
-    maxsupply: number,
-    blockreward: number,
-    exchangerate: number,
+    maxSupply: number,
+    blockReward: number,
+    exchangeRate: number,
     currency: string,
     decimals: number,
-    txfee: number,
-    storedfees: number
+    txFee: number,
+    storedFees: number
 }
 
 export interface ISettings {
     prefix: string,
     token: string,
     guildId: string
-}
-
-export interface IUser {
-    accounts: IAccount[];
 }
 
 export interface IAccount {
