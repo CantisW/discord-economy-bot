8a9bf6b86d90c8f9eea1eeced9038820006367e1
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2022-02-19 13:28:42 -0800

    chore: update errors

--- src/util/errors.ts
+++ src/util/errors.ts
@@ -1,4 +1,5 @@
 export const ERRORS = {
     ACCOUNT_ALREADY_EXISTS: "You already have an account!",
-    ACCOUNT_DOES_NOT_EXIST: "You do not have an account!"
+    ACCOUNT_DOES_NOT_EXIST: "You do not have an account!",
+    ACCOUNT_CANNOT_CREATE: "Your account couldn't be created. Please try again later."
 }
