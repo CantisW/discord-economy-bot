2e3f266a420820c84d596472812e8bb826cd99a0
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2022-02-19 18:17:35 -0800

    chore: updates errors

--- src/util/errors.ts
+++ src/util/errors.ts
@@ -1,5 +1,9 @@
 export const ERRORS = {
+    GENERIC_ERROR: "Something unexpected has occured. Please try again later.",
     ACCOUNT_ALREADY_EXISTS: "You already have an account!",
     ACCOUNT_DOES_NOT_EXIST: "You do not have an account!",
-    ACCOUNT_CANNOT_CREATE: "Your account couldn't be created. Please try again later."
+    ACCOUNT_CANNOT_CREATE: "Your account couldn't be created. Please try again later.",
+    BALANCE_CANNOT_RETRIEVE: "This balance cannot be retrieved!",
+    CONVERT_INPUT_VALID_AMOUNT: "Please input a valid amount!",
+    SUPPLY_WILL_BE_EXCEEDED: "The max supply has been reached!",
 }
