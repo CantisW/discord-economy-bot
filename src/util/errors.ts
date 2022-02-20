05001dcf29159f2957433e9e7eb24934c01b9068
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2022-02-19 20:58:59 -0800

    chore: update errors

--- src/util/errors.ts
+++ src/util/errors.ts
@@ -3,7 +3,9 @@ export const ERRORS = {
     ACCOUNT_ALREADY_EXISTS: "You already have an account!",
     ACCOUNT_DOES_NOT_EXIST: "You do not have an account!",
     ACCOUNT_CANNOT_CREATE: "Your account couldn't be created. Please try again later.",
-    BALANCE_CANNOT_RETRIEVE: "This balance cannot be retrieved!",
+    ACCOUNT_CANNOT_RETRIEVE: "This account does not exist!",
+    BALANCE_CANNOT_RETRIEVE: "There is no balance to show.",
     CONVERT_INPUT_VALID_AMOUNT: "Please input a valid amount!",
     SUPPLY_WILL_BE_EXCEEDED: "The max supply has been reached!",
+    TRANSACTION_AMOUNT_INVALID: "You do not have enough currency!"
 }
