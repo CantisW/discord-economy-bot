52f8a1ddfa49b128bd416ac826ea3b9943c01569
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2022-02-19 22:23:42 -0800

    feat: cooldowns

--- src/util/bot.ts
+++ src/util/bot.ts
@@ -1,11 +1,18 @@
 import fs from "fs";
-import { IConfig } from "./types";
+import { IConfig, ICooldown, ISettings } from "./types";
+
+let cooldowns: ICooldown[] = [];
 
 export const getConfig = (): IConfig => {
     let config = fs.readFileSync("./src/data/settings.json","utf-8");
     return JSON.parse(config);
 }
 
+/**
+ * Write to a certain config property.
+ * @param property 
+ * @param write 
+ */
 export const WriteToConfig = (property: string, write: string | number) => {
     let config = getConfig()
 
@@ -24,4 +31,32 @@ export const returnSetting = (setting: string) => {
 
 export const sanitizeId = (id: string) => {
     return id.replace(/[\\<>@#&!]/g, "");
+}
+
+/**
+ * Checks for command cooldown. If false, make a cooldown.
+ * @param command 
+ * @param time 
+ * @param author 
+ * @returns boolean
+ */
+export const doCooldown = (command: string, time: number, author: string) => {
+    let found = false;
+    cooldowns.forEach((v, i) => {
+        if (v.command === command && v.author === author) {
+            found = true;
+        }
+    })
+    if (found) return true;
+    let obj: ICooldown = { command: command, time: time, author: author };
+    cooldowns.push(obj);
+    setTimeout(() => {
+        cooldowns = cooldowns.filter(v => v !== obj)
+    }, time)
+    return false;
+}
+
+export const getUnit = (cooldown: number) => {
+    if (cooldown >= 60000) return "minutes";
+    return "seconds";
 }
