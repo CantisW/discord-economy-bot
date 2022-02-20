f4b75527b79f7e1827255a9c98297ea4a5587620
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2022-02-19 18:17:00 -0800

    feat: view other account balances

---
+++ src/commands/balance.ts
@@ -0,0 +1,25 @@
+import { CommandInteraction } from "discord.js";
+import { Discord, Slash, SlashOption } from "discordx";
+import { parseDecimals } from "../util/blockchain.js";
+import { sanitizeId, getConfig } from "../util/bot.js";
+import { ERRORS } from "../util/errors.js";
+import { getAccountBalance } from "../util/users.js";
+
+@Discord()
+export class Balance {
+    @Slash("balance", { description: "See your own or someone else's balance." })
+    async balance(
+        @SlashOption("user", { type: "STRING", description: "Who? [ UserId or @User ]" })
+        user: string,
+        interaction: CommandInteraction
+    ) {
+        let { ticker, exchangeRate, currency } = getConfig();
+
+        let parsed = sanitizeId(user);
+        let bal = await getAccountBalance(parsed);
+        if (!bal) {
+            return interaction.reply(ERRORS.BALANCE_CANNOT_RETRIEVE)
+        }
+        interaction.reply(`${parsed} has ${bal} ${ticker} (${parseDecimals(exchangeRate * bal)} ${currency})`)
+    }
+}
