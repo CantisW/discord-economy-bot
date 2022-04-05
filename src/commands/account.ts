9dd93aaded35687a1390a813fd325ac0f5302652
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2022-04-05 16:19:23 -0700

    feat: locales list

--- src/commands/account.ts
+++ src/commands/account.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction } from "discord.js";
+import { ColorResolvable, CommandInteraction, MessageEmbed } from "discord.js";
 import { Discord, Permission, Slash, SlashChoice, SlashOption } from "discordx";
 import { getConfig } from "../util/bot.js";
 import {
@@ -6,6 +6,7 @@ import {
     checkIfAccountExists,
     createAccount,
     getAccountBalance,
+    getAllLocales,
     getLocale,
     lang,
 } from "../util/users.js";
@@ -54,16 +55,36 @@ export class Account {
 
     @Slash("locale", { description: "Define your localization. This will only change command replies." })
     async locale(
-        @SlashOption("locale", { type: "STRING", description: "[ string ]", required: false})
+        @SlashOption("locale", { type: "STRING", description: "Pick a locale from the list. [ string ]", required: false})
         locale: string,
         interaction: CommandInteraction
     ) {
+        const embed = new MessageEmbed()
+                .setColor('0xf1c40f' as ColorResolvable)
+                .setTitle(await lang(`LOCALES_LIST_TITLE`, interaction.user.id))
+                //.setURL('')s
+                //.setAuthor('Santeeisweird9')
+                .setDescription(await lang(`LOCALES_LIST_DESC`, interaction.user.id))
+
         if (!locale) {
             let { localeKey, localeName } = await getLocale(interaction.user.id);
             return interaction.reply(await lang(`CURRENT_LOCALE`, interaction.user.id, [ localeName, localeKey ]))
         }
-        changeLocale(interaction.user.id, locale).then(async () => {
-            interaction.reply(await lang(`LOCALE_CHANGED`, interaction.user.id))
-        }).catch((err) => interaction.reply(err))
+        if (locale === "list") {
+            const locales = await getAllLocales();
+            const length = locales.length;
+
+            for (let i = 0; i<length; i++) {
+                embed.addFields({
+                    name: locales[i].localeKey,
+                    value: locales[i].localeName
+                })
+            }
+            return interaction.reply({ embeds: [embed] })
+        } else {
+            changeLocale(interaction.user.id, locale).then(async () => {
+                interaction.reply(await lang(`LOCALE_CHANGED`, interaction.user.id))
+            }).catch((err) => interaction.reply(err))
+        }
     }
 }
